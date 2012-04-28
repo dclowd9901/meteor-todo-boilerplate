496a3d38dbe425f594ab89fec93bfcc085da355a
dclowd9901 <david.ddrew@gmail.com>
2012-04-28 15:47:25 -0700

    working iteration

--- client/todo.js
+++ client/todo.js
@@ -1,14 +1,40 @@
+var Lists = new Meteor.Collection('lists');
 var Tasks = new Meteor.Collection("tasks");
 
 Template.list.events = {
-    "click #add": function () {
-        var roomName = window.prompt("Name for the new name", "My room");
-        if(roomName != "") {
-            Rooms.insert({"name": roomName});
-        }
-    }
+    "click #add-task": function(e) {
+		e.preventDefault();
+		addTask();
+    },
+	"keyup #new-task": function(e){
+		e.preventDefault();		
+	},
+	"submit form": function(e){
+		e.preventDefault();
+		addTask();
+	},
+	"click .remove": function(e){
+		e.preventDefault();
+		var _id = $(e.target).data('id'); 
+		if( _id ){
+			Tasks.remove( _id );
+		}
+	}
 };
 
 Template.list.tasks = function(){
 	return Tasks.find({});
+}
+
+function addTask(){
+	var taskName = $('#new-task').val();
+    if(taskName != "") {
+        Tasks.insert({ name : taskName });
+		$('#new-task').val('');
+    }
+}
+
+function upsertList(){
+	var listName = $('#new-task').val();
+	
 }
--- server/todo.js
+++ server/todo.js
@@ -1,1 +1,6 @@
+var Lists = new Meteor.Collection('lists');
 var Tasks = new Meteor.Collection('tasks');
+
+Meteor.startup(function () {
+    // code to run on server at startup
+});
