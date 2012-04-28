f1161e3ef9133a9e4b3726d10384470384c70b52
dclowd9901 <david.ddrew@gmail.com>
2012-04-28 16:37:33 -0700

    User can create new lists.

--- client/todo.js
+++ client/todo.js
@@ -19,22 +19,56 @@ Template.list.events = {
 		if( _id ){
 			Tasks.remove( _id );
 		}
+	},
+	"click #new-list" : function(e){
+		e.preventDefault();
+		Session.set('list', undefined);
 	}
 };
 
+Template.getList.events = {
+	"click #submit-get-list" : function(e){
+		e.preventDefault();
+		getsertList();
+	},
+	"submit form" : function(e){
+		e.preventDefault();
+		getsertList();
+	}
+}
+
+Template.main.currentList = function() {
+    return Session.get("list") || false;
+}
+
 Template.list.tasks = function(){
-	return Tasks.find({});
+	return Tasks.find({ list : Session.get('list') });
+}
+
+Template.list.currentList = function(){
+    var list = Lists.findOne({_id: Session.get("list")});
+    return list && list.name ;
 }
 
 function addTask(){
 	var taskName = $('#new-task').val();
     if(taskName != "") {
-        Tasks.insert({ name : taskName });
+        Tasks.insert({ name : taskName, list : Session.get('list') });
 		$('#new-task').val('');
     }
 }
 
-function upsertList(){
-	var listName = $('#new-task').val();
-	
+function getsertList(){
+	var listName = $('#get-list').val();
+	if( listName ){
+		var list = Lists.findOne({ name : listName });
+		
+		if( !list ){
+			var _id = Lists.insert({ name : listName });
+			Session.set( "list", _id );					
+		} else {
+			Session.set( "list", list._id );
+		}
+
+	}
 }
